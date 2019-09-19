2a37929ec45bc6412ac89ed0edb93f6d562c57cd
Marc <marcdwest@gmail.com>
2019-09-19 13:22:28 -0500

    add loading spinner. reconfigure Users from class to function

--- src/components/users/Users.js
+++ src/components/users/Users.js
@@ -1,24 +1,31 @@
-import React, { Component } from 'react';
+import React from 'react';
 import UserItem from './UserItem';
+import Spinner from '../layout/Spinner';
+import PropTypes from 'prop-types';
 
 const userStyle = {
-  display: 'grid',
-  gridTemplateColumns: 'repeat(3, 1fr)',
-  gridGap: '1rem',
-}
+	display: 'grid',
+	gridTemplateColumns: 'repeat(3, 1fr)',
+	gridGap: '1rem',
+};
 
-class Users extends Component {
+const Users = ({ users, loading }) => {
+	if (loading) {
+		return <Spinner />;
+	} else {
+		return (
+			<div style={userStyle}>
+				{users.map(user => (
+					<UserItem key={user.id} user={user} />
+				))}
+			</div>
+		);
+	}
+};
 
-  render() {
-    return (
-      <div style={userStyle}>
-        {this.props.users.map(user => (
-          <UserItem key={user.id} user={user} />
-        ))}
-      </div>
-    )
-  }
-  
-}
+Users.propTypes = {
+	users: PropTypes.array.isRequired,
+	loading: PropTypes.bool.isRequired,
+};
 
-export default Users
+export default Users;
