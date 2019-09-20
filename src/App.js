0e534832017981a6da3461aa1510b05578509c15
Marc <marcdwest@gmail.com>
2019-09-19 22:27:55 -0500

    searchUsers functional. remove auto-load profiles

--- src/App.js
+++ src/App.js
@@ -6,38 +6,53 @@ import axios from 'axios';
 import './App.css';
 
 class App extends Component {
-  state = {
-    users: [],
-    loading: false,
-  }
-  
-  async componentDidMount() {
-    this.setState({
-      loading: true,
-    });
+	state = {
+		users: [],
+		loading: false,
+	};
 
-    const res = await axios.get(`https://api.github.com/users?
-      client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&
-      client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
-    
-    this.setState({
-      users: res.data,
-      loading: false,
-    })
-  }
-  
-  render() {
-    return (
-      <div className="App">
-        <Navbar />
-        <div className="container">
-          <Search />
-          <Users loading={this.state.loading} users={this.state.users} />
-        </div>
-      </div>
-    );
-  }
-  
+	// async componentDidMount() {
+	// 	this.setState({
+	// 		loading: true,
+	// 	});
+
+	// 	const res = await axios.get(`https://api.github.com/users?
+  //     client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}
+  //     &client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
+
+	// 	this.setState({
+	// 		users: res.data,
+	// 		loading: false,
+	// 	});
+	// }
+
+	//Search Github users
+	searchUsers = async text => {
+		this.setState({
+			loading: true,
+		});
+
+		const res = await axios.get(`https://api.github.com/search/users?q=${text}
+      &client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}
+      &client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
+
+		this.setState({
+			users: res.data.items,
+			loading: false,
+		});
+	};
+
+	render() {
+		return (
+			<div className='App'>
+				<Navbar />
+				<div className='container'>
+					<Search searchUsers={this.searchUsers} />
+					<Users loading={this.state.loading} users={this.state.users} />
+				</div>
+			</div>
+		);
+	}
 }
 
 export default App;
